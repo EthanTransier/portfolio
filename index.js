ca424a9d95f2f3064bf7ddd673eece3cf2c05135
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2022-12-09 18:22:45 -0700

    Changed stuff

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ function sideBarAnimation(){
     if(sideBarState == 1){
         sideBarState = 2;
         document.getElementById("sideBar").classList.add("sideBarOpenAnimationClass");
+        document.getElementById("sideBar").classList.remove("sideBarCloseAnimationClass");
+    }else if(sideBarState == 2){
+        sideBarState = 1;
+        document.getElementById("sideBar").classList.remove("sideBarOpenAnimationClass");
+        document.getElementById("sideBar").classList.add("sideBarCloseAnimationClass");
     }
 }
