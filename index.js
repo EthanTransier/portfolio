84b4d71d357ad967df7025883a465b0ab9f0c13c
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2022-12-12 18:24:17 -0700

    Added containers to html/css home page
    Started The Project, Basic Style Form has been set

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-var sideBarState = 1;
-
-function sideBarAnimation(){
-    if(sideBarState == 1){
-        sideBarState = 2;
-        document.getElementById("sideBar").classList.add("sideBarOpenAnimationClass");
-        document.getElementById("sideBar").classList.remove("sideBarCloseAnimationClass");
-    }else if(sideBarState == 2){
-        sideBarState = 1;
-        document.getElementById("sideBar").classList.remove("sideBarOpenAnimationClass");
-        document.getElementById("sideBar").classList.add("sideBarCloseAnimationClass");
+function scrolled(){
+    console.log(window.scrollY)
+    if(window.scrollY >= 100){
+        console.log("worked")
+        document.getElementById("infoBoxHiderID").classList.add("appearAnimation");
     }
 }
