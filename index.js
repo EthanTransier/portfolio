acaf566c55d5583f73360fee82eb610f7c984c3d
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2022-12-13 15:12:38 -0700

    Added more to the About Me Page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function scrolled(){
     console.log(window.scrollY)
-    if(window.scrollY >= 100){
+    if(window.scrollY >= 50){
         console.log("worked")
         document.getElementById("infoBoxHiderID").classList.add("appearAnimation");
     }
